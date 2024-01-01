0e8c9336498cff8bb75cdd5dff342e1ea7367044
RozsahegyiAkos <akos.rozsahegyi@katonaj-mkszig.hu>
2024-01-01 11:18:38 +0100

    Done 2024-01-01

--- juice.js
+++ juice.js
@@ -1,8 +1,33 @@
+body = document.querySelector('body')
 button = document.querySelector('button')
 target = document.querySelector('.target')
-sniper = document.querySelector('.sniper')
+cursor = document.querySelector('.cursor')
 
-window.addEventListener('mousemove',function(e){
-    sniper.style.left = e.pageX + "px";
-    sniper.style.top = e.pageY + "px";
+score = 0;
+screenWidth = body.offsetWidth;
+screenHeight = body.offsetHeight;
+
+button.addEventListener('click',function(){
+
+    window.addEventListener('mousemove',function(e){
+        cursor.style.left = e.pageX + "px";
+        cursor.style.top = e.pageY + "px"; 
+        console.log(e.target.classList)
+    })
+
+    window.addEventListener('click',function(e){
+     
+        if(e.target.classList.contains("target")){
+            score++;
+            button.innerHTML = "Score "+score;
+        }
+    }) 
+
+    setInterval(function(){
+        randTop = Math.random() * (screenHeight - 150);
+        randleft = Math.random() * (screenWidth - 150);
+        target.style.left = randleft + "px";
+        target.style.top = (randTop) + "px";
+
+        },1000)
 })
